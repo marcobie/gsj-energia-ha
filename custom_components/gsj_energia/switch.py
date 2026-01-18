105551e355df83afa949673b1bd69ef48f641971
Marcin Obierzyński <58610604+marcobie@users.noreply.github.com>
2026-01-18 20:28:16 +0100

    Create switch.py

---
+++ custom_components/gsj_energia/switch.py
@@ -0,0 +1,34 @@
+from homeassistant.components.switch import SwitchEntity
+
+from .const import DOMAIN
+
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    data = hass.data[DOMAIN]
+    coordinator = data["coordinator"]
+    client = data["client"]
+
+    async_add_entities([GSJCWUSwitch(coordinator, client)])
+
+
+class GSJCWUSwitch(SwitchEntity):
+    def __init__(self, coordinator, client):
+        self.coordinator = coordinator
+        self.client = client
+        self._attr_name = "GSJ CWU"
+        self._attr_unique_id = "gsj_cwu_switch"
+
+    @property
+    def is_on(self):
+        return str(self.coordinator.data.get("CWU_STATUS", "0")) == "1"
+
+    async def async_turn_on(self, **kwargs):
+        await self.client.set_value("CWU_STATUS", 1)
+        await self.coordinator.async_request_refresh()
+
+    async def async_turn_off(self, **kwargs):
+        await self.client.set_value("CWU_STATUS", 0)
+        await self.coordinator.async_request_refresh()
+
+    async def async_update(self):
+        await self.coordinator.async_request_refresh()
