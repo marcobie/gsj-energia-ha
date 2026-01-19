b4484d46debca97d5a6a789ab6dc40afc61dcec7
Marcin Obierzyński <58610604+marcobie@users.noreply.github.com>
2026-01-19 19:57:21 +0100

    Update coordinator.py

--- custom_components/gsj_energia/coordinator.py
+++ custom_components/gsj_energia/coordinator.py
@@ -1,30 +1,28 @@
 from datetime import timedelta
 import logging
 
-from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from homeassistant.core import HomeAssistant
 
 from .api import GSJClient
-from .const import DOMAIN
+from .const import DOMAIN, DEFAULT_SCAN_INTERVAL
 
 _LOGGER = logging.getLogger(__name__)
 
 
-class GSJDataUpdateCoordinator(DataUpdateCoordinator):
-    def __init__(self, hass: HomeAssistant, client: GSJClient, update_interval: int = 60):
+class GSJEnergiaCoordinator(DataUpdateCoordinator):
+    def __init__(self, hass: HomeAssistant, client: GSJClient, scan_interval: int = DEFAULT_SCAN_INTERVAL):
         self.client = client
 
         super().__init__(
             hass,
             _LOGGER,
             name=DOMAIN,
-            update_interval=timedelta(seconds=update_interval),
+            update_interval=timedelta(seconds=scan_interval),
         )
 
     async def _async_update_data(self):
         try:
-            data = await self.client.get_status()
-            _LOGGER.debug("Pobrane dane z GSJ Browser API: %s", data)
-            return data
+            return await self.client.get_status()
         except Exception as err:
             raise UpdateFailed(f"Błąd komunikacji z GSJ Browser API: {err}") from err
