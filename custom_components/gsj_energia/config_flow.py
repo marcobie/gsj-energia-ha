f8ceacf8d6a641a1a02a015b1d41d0eef7c7e9c9
Marcin Obierzyński <58610604+marcobie@users.noreply.github.com>
2026-01-19 19:03:41 +0100

    Update config_flow.py

--- custom_components/gsj_energia/config_flow.py
+++ custom_components/gsj_energia/config_flow.py
@@ -2,30 +2,71 @@ from __future__ import annotations
 
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.data_entry_flow import FlowResult
+from homeassistant.core import callback
 
-from .const import DOMAIN, CONF_HOST, CONF_PORT, CONF_DEVICE_ID, DEFAULT_PORT
+from .const import (
+    DOMAIN,
+    CONF_HOST,
+    CONF_PORT,
+    CONF_DEVICE_ID,
+    CONF_SCAN_INTERVAL,
+    DEFAULT_PORT,
+    DEFAULT_SCAN_INTERVAL,
+)
 
-
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class GSJEnergiaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
-    async def async_step_user(self, user_input=None) -> FlowResult:
+    async def async_step_user(self, user_input=None):
+        errors = {}
+
         if user_input is not None:
             return self.async_create_entry(
                 title="GSJ Energia",
                 data=user_input,
             )
 
-        data_schema = vol.Schema(
+        schema = vol.Schema(
             {
                 vol.Required(CONF_HOST, default="localhost"): str,
                 vol.Required(CONF_PORT, default=DEFAULT_PORT): int,
                 vol.Required(CONF_DEVICE_ID, default=414): int,
+                vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): int,
             }
         )
 
         return self.async_show_form(
             step_id="user",
-            data_schema=data_schema,
+            data_schema=schema,
+            errors=errors,
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return GSJEnergiaOptionsFlow(config_entry)
+
+
+class GSJEnergiaOptionsFlow(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        schema = vol.Schema(
+            {
+                vol.Required(
+                    CONF_SCAN_INTERVAL,
+                    default=self.config_entry.options.get(
+                        CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
+                    ),
+                ): int
+            }
+        )
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=schema,
         )
