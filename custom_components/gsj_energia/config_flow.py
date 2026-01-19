f41434cef4f3a3a9bb0a114b174465c70570e6c8
Marcin Obierzyński <58610604+marcobie@users.noreply.github.com>
2026-01-19 18:44:44 +0100

    Update config_flow.py

--- custom_components/gsj_energia/config_flow.py
+++ custom_components/gsj_energia/config_flow.py
@@ -2,31 +2,22 @@ from __future__ import annotations
 
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
 
-from .const import (
-    DOMAIN,
-    CONF_HOST,
-    CONF_PORT,
-    CONF_DEVICE_ID,
-    DEFAULT_PORT,
-)
+from .const import DOMAIN, CONF_HOST, CONF_PORT, CONF_DEVICE_ID, DEFAULT_PORT
 
 
-class GSJEnergiaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None) -> FlowResult:
-        errors = {}
-
         if user_input is not None:
             return self.async_create_entry(
                 title="GSJ Energia",
                 data=user_input,
             )
 
-        schema = vol.Schema(
+        data_schema = vol.Schema(
             {
                 vol.Required(CONF_HOST, default="localhost"): str,
                 vol.Required(CONF_PORT, default=DEFAULT_PORT): int,
@@ -36,6 +27,5 @@ class GSJEnergiaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(
             step_id="user",
-            data_schema=schema,
-            errors=errors,
+            data_schema=data_schema,
         )
