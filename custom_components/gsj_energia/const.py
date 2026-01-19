ec7bb9dfc7c70c61a39c4a08ce491e3d669f0390
Marcin Obierzyński <58610604+marcobie@users.noreply.github.com>
2026-01-19 18:12:15 +0100

    Update const.py

--- custom_components/gsj_energia/const.py
+++ custom_components/gsj_energia/const.py
@@ -1,13 +1,7 @@
 DOMAIN = "gsj_energia"
 
-CONF_HOST = "host"
-CONF_USERNAME = "username"
-CONF_PASSWORD = "password"
-CONF_DEVICE_ID = "device_id"
-CONF_SCAN_INTERVAL = "scan_interval"
-
-DEFAULT_SCAN_INTERVAL = 60
-
-LOGIN_PATH = "/login"
-SET_CACHE_PATH = "/set-user-cache"
-GET_CACHE_PATH = "/get-user-cache"
+PLATFORMS = [
+    "sensor",
+    "switch",
+    "number",
+]
