c9a60d74b5504cd689d7c757c4cb8fd0fe7adf55
Marcin Obierzyński <58610604+marcobie@users.noreply.github.com>
2026-01-19 18:22:29 +0100

    Update const.py

--- custom_components/gsj_energia/const.py
+++ custom_components/gsj_energia/const.py
@@ -1,5 +1,13 @@
 DOMAIN = "gsj_energia"
 
+CONF_HOST = "host"
+CONF_PORT = "port"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+CONF_DEVICE_ID = "device_id"
+
+DEFAULT_PORT = 8124
+
 PLATFORMS = [
     "sensor",
     "binary_sensor",
