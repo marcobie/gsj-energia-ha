40848f7c82bf1922671837e54cbf0845eef388dc
Marcin Obierzyński <58610604+marcobie@users.noreply.github.com>
2026-01-19 18:30:01 +0100

    Update config_flow.py

--- custom_components/gsj_energia/config_flow.py
+++ custom_components/gsj_energia/config_flow.py
@@ -1,15 +1,23 @@
+from __future__ import annotations
+
 import voluptuous as vol
-
 from homeassistant import config_entries
-from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD
+from homeassistant.core import HomeAssistant
+from homeassistant.data_entry_flow import FlowResult
 
-from .const import DOMAIN, CONF_DEVICE_ID, CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
+from .const import (
+    DOMAIN,
+    CONF_HOST,
+    CONF_PORT,
+    CONF_DEVICE_ID,
+    DEFAULT_PORT,
+)
 
 
-class GSJConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class GSJEnergiaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
-    async def async_step_user(self, user_input=None):
+    async def async_step_user(self, user_input=None) -> FlowResult:
         errors = {}
 
         if user_input is not None:
@@ -20,11 +28,9 @@ class GSJConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         schema = vol.Schema(
             {
-                vol.Required(CONF_HOST, default="gsj-energia.com"): str,
-                vol.Required(CONF_USERNAME): str,
-                vol.Required(CONF_PASSWORD): str,
+                vol.Required(CONF_HOST, default="localhost"): str,
+                vol.Required(CONF_PORT, default=DEFAULT_PORT): int,
                 vol.Required(CONF_DEVICE_ID, default=414): int,
-                vol.Optional(CONF_SCAN_INTERVAL, default=60): int,
             }
         )
 
