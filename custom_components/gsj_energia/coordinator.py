b5629c0433b76c4e17eadcac6a50f3f95dc76e50
Marcin Obierzyński <58610604+marcobie@users.noreply.github.com>
2026-01-19 17:50:31 +0100

    Update coordinator.py

--- custom_components/gsj_energia/coordinator.py
+++ custom_components/gsj_energia/coordinator.py
@@ -1,52 +1,30 @@
 from datetime import timedelta
 import logging
 
-from homeassistant.helpers.update_coordinator import (
-    DataUpdateCoordinator,
-    UpdateFailed,
-)
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
+from .api import GSJClient
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-# Lista kluczy, które cyklicznie odczytujemy z API
-GSJ_KEYS = [
-    "TEMPERATURA_ZASIL",
-    "TEMPERATURA_BUFOR",
-    "TEMPERATURA_CWU",
-    "TEMPERATURA_ZEWN",
-    "TEMP_PAROWNIKA",
-    "TEMP_GAZ_PAROWANIE",
-    "GRZANIE_ZADANA",
-    "CWU_ZADANA",
-    "CO_STATUS",
-    "CWU_STATUS",
-    "SSH_TRYG",
-    "ALARM",
-]
-
 
 class GSJDataUpdateCoordinator(DataUpdateCoordinator):
-    def __init__(self, hass, client, scan_interval):
+    def __init__(self, hass: HomeAssistant, client: GSJClient, update_interval: int = 60):
         self.client = client
 
         super().__init__(
             hass,
             _LOGGER,
             name=DOMAIN,
-            update_interval=timedelta(seconds=scan_interval),
+            update_interval=timedelta(seconds=update_interval),
         )
 
     async def _async_update_data(self):
-        """Pobierz wszystkie dane z GSJ."""
-        data = {}
-
         try:
-            for key in GSJ_KEYS:
-                value = await self.client.get_value(key)
-                data[key] = value
+            data = await self.client.get_status()
+            _LOGGER.debug("Pobrane dane z GSJ Browser API: %s", data)
             return data
-
         except Exception as err:
-            raise UpdateFailed(f"Błąd komunikacji z GSJ: {err}") from err
+            raise UpdateFailed(f"Błąd komunikacji z GSJ Browser API: {err}") from err
