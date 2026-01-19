3ca4871bbd557d3bf86db045719ad9fdede62051
Marcin Obierzyński <58610604+marcobie@users.noreply.github.com>
2026-01-19 19:17:23 +0100

    Update const.py

--- custom_components/gsj_energia/const.py
+++ custom_components/gsj_energia/const.py
@@ -1,11 +1,16 @@
 DOMAIN = "gsj_energia"
 
+# Platformy HA
+PLATFORMS = ["sensor", "binary_sensor", "climate", "number", "switch"]
+
+# Konfiguracja
 CONF_HOST = "host"
 CONF_PORT = "port"
 CONF_DEVICE_ID = "device_id"
 CONF_SCAN_INTERVAL = "scan_interval"
 
+# Domyślne wartości
+DEFAULT_HOST = "localhost"
 DEFAULT_PORT = 8124
+DEFAULT_DEVICE_ID = 414
 DEFAULT_SCAN_INTERVAL = 60
-
-PLATFORMS = ["sensor", "binary_sensor", "climate", "number", "switch"]
