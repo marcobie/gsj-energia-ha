63439ce0ca84a67c9d1e0e8c4a8d30adb58f54ed
Marcin Obierzyński <58610604+marcobie@users.noreply.github.com>
2026-01-19 19:57:49 +0100

    Update config_flow.py

--- custom_components/gsj_energia/config_flow.py
+++ custom_components/gsj_energia/config_flow.py
@@ -1,6 +1,5 @@
-from __future__ import annotations
-
 import voluptuous as vol
+
 from homeassistant import config_entries
 from homeassistant.core import callback
 
@@ -10,43 +9,38 @@ from .const import (
     CONF_PORT,
     CONF_DEVICE_ID,
     CONF_SCAN_INTERVAL,
-    DEFAULT_HOST,
     DEFAULT_PORT,
-    DEFAULT_DEVICE_ID,
     DEFAULT_SCAN_INTERVAL,
 )
 
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+
+class GSJEnergiaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        errors = {}
+
         if user_input is not None:
-            return self.async_create_entry(
-                title="GSJ Energia",
-                data=user_input,
-            )
+            return self.async_create_entry(title="GSJ Energia", data=user_input)
 
         schema = vol.Schema(
             {
-                vol.Required(CONF_HOST, default=DEFAULT_HOST): str,
-                vol.Required(CONF_PORT, default=DEFAULT_PORT): int,
-                vol.Required(CONF_DEVICE_ID, default=DEFAULT_DEVICE_ID): int,
+                vol.Required(CONF_HOST): str,
+                vol.Optional(CONF_PORT, default=DEFAULT_PORT): int,
+                vol.Required(CONF_DEVICE_ID): int,
                 vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): int,
             }
         )
 
-        return self.async_show_form(
-            step_id="user",
-            data_schema=schema,
-        )
+        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
 
     @staticmethod
     @callback
     def async_get_options_flow(config_entry):
-        return OptionsFlowHandler(config_entry)
+        return GSJEnergiaOptionsFlowHandler(config_entry)
 
 
-class OptionsFlowHandler(config_entries.OptionsFlow):
+class GSJEnergiaOptionsFlowHandler(config_entries.OptionsFlow):
     def __init__(self, config_entry):
         self.config_entry = config_entry
 
@@ -56,16 +50,11 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
 
         schema = vol.Schema(
             {
-                vol.Required(
+                vol.Optional(
                     CONF_SCAN_INTERVAL,
-                    default=self.config_entry.options.get(
-                        CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
-                    ),
+                    default=self.config_entry.options.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL),
                 ): int
             }
         )
 
-        return self.async_show_form(
-            step_id="init",
-            data_schema=schema,
-        )
+        return self.async_show_form(step_id="init", data_schema=schema)
